cdf86c8a6cbdc8af31d5f25a5cd13078357c7445
chrischan8551 <chrischan8551@gmail.com>
2025-05-19 12:41:41 -0500

    1

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,13 +1,15 @@
+import ProductEmail from "@/app/components/ProductEmail";
 import { stripe } from "@/app/lib/stripe";
-import { headers } from "next/headers";
-import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+import { headers } from "next/headers";
+// import { Resend } from "resend";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
     const body = await req.text();
-    const headersList = await headers();
-    const signature = headersList.get("Stripe-Signature") as string;
+
+    const signature = (await headers()).get("Stripe-Signature") as string;
 
     let event;
 
@@ -24,24 +26,17 @@ export async function POST(req: Request) {
     switch (event.type) {
         case "checkout.session.completed": {
             const session = event.data.object;
+
             const link = session.metadata?.link;
 
-            // Send email without directly using React components in API route
-            const { data, error } = await resend.emails.send({
-                from: "Digital Marketplace <onboarding@resend.dev>",
-                to: ["your_email"],
-                subject: "Your Product from Digital Marketplace",
-                html: `
-                <div>
-                    <h1>Hi Friend,</h1>
-                    <p>Thank you for buying your product at Digital Marketplace</p>
-                    <div style="text-align: center; margin-top: 24px;">
-                        <a href="${link}" style="background-color: #3b82f6; color: white; padding: 10px 20px; border-radius: 8px; text-decoration: none;">Your Download Link</a>
-                    </div>
-                    <p>Best,<br/>Digital Marketplace Team</p>
-                </div>
-                `,
-            });
+            // const { data, error } = await resend.emails.send({
+            //     from: "MarshalUI <onboarding@resend.dev>",
+            //     to: ["your_email"],
+            //     subject: "Your Product from MarshalUI",
+            //     react: ProductEmail({
+            //         link: link as string,
+            //     }),
+            // });
 
             break;
         }
